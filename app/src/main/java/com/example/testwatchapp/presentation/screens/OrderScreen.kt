f13ed844f700a5171d90716bd7e8ecf6a05135b3
Admin <life_paradishe@mail.ru>
2022-12-20 14:01:10 +0300

    fix 5

--- app/src/main/java/com/example/testwatchapp/presentation/screens/OrderScreen.kt
+++ app/src/main/java/com/example/testwatchapp/presentation/screens/OrderScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -25,7 +24,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.testwatchapp.R
 import androidx.navigation.NavHostController
-import androidx.wear.compose.material.Card
 import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.Text
 import coil.compose.AsyncImage
@@ -35,7 +33,6 @@ import com.example.testwatchapp.presentation.model.OrderGetModel
 import com.example.testwatchapp.presentation.storage.Token
 import com.example.testwatchapp.presentation.theme.Nutino
 import com.example.testwatchapp.presentation.theme.Orange1
-import com.example.testwatchapp.presentation.theme.Orange2
 import com.example.testwatchapp.presentation.theme.Roboto
 import retrofit2.Call
 import retrofit2.Callback
